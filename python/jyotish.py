6f49ed559fda865cc730ac677b6fdb455fcde71c
SanjayPrabhakaran <sanjaychettiar@gmail.com>
2025-09-10 18:00:57 +0530

    jyotish.py

--- python/jyotish.py
+++ python/jyotish.py
@@ -1,8 +1,6 @@
 debug=True
 if debug : print("Loaded Module:",__name__) 
 
-bha_sphuta = lambda x : (x+720)%360
-thithi_sphuta = lambda sun, moon : bha_sphuta(moon-sun)
 
 # Define a class for the chart
 
@@ -12,12 +10,12 @@ from dataclasses import dataclass, field
 @dataclass
 class Chart:
     lagna: float = 0.0  # Ascendant or rising sign
-    chandra: float = 0.0  # Position of the Moon
     surya: float = 0.0  # Position of the Sun
-    budha: float = 0.0  # Position of Mercury
-    shukra: float = 0.0  # Position of Venus
+    chandra: float = 0.0  # Position of the Moon
     mangal: float = 0.0  # Position of Mars
+    budha: float = 0.0  # Position of Mercury
     guru: float = 0.0  # Position of Jupiter
+    shukra: float = 0.0  # Position of Venus
     shani: float = 0.0  # Position of Saturn
     rahu: float = 0.0  # Position of the North Lunar Node
     ketu: float = 0.0  # Position of the South Lunar Node
@@ -32,14 +30,32 @@ class Jataka:
     deshamsha: float =0
     # Add more fields as needed
 
-dX = lambda s,X : bha_sphuta(s*X)
-d20 = lambda s : dX(s,20)
- 
+bha_sphuta = lambda x : (x+720)%360 #normalize to 0-360
+thithi_sphuta = lambda sun, moon : bha_sphuta(moon-sun) #thithi calculation
+rashi = lambda x : int(bha_sphuta(x)/30)+1 #rashi calculation returns an integer 1-12
+rashi_sesha = lambda x : int((int(x+0.999999)-1)%12+1) # normalize rashi to 1-12
+dXrashi = lambda s,X : rashi(s*X)
+d20rashi = lambda s : dXrashi(s,20)
+PranaPada = lambda ishta,surya : rashi_sesha((ishta*60/15)+d20rashi(surya))
+yoga_rashi = lambda r1,r2 : int(int(r1+0.99999)+int(r2+0.999999)-1)
+
 #my chart
 jataka=Jataka()
 jataka.chart.lagna = 137.05747588523033
 jataka.ishta = 7.9583
-print(jataka,d20(jataka.chart.lagna))
+jataka.chart.surya = 92.378
+jataka.chart.chandra = 53.01
+jataka.chart.mangal = 298.093
+jataka.chart.budha = 117.106
+jataka.chart.guru = 213.181
+jataka.chart.shukra = 81.513
+jataka.chart.shani = 39.864
+jataka.chart.rahu = 291.947
+jataka.chart.ketu = bha_sphuta(jataka.chart.rahu+180)
+
+
 
 if __name__ == "__main__" :
     print("Direct Run")
+    print(jataka,"D20 Lagna=",d20rashi(jataka.chart.lagna),"\nPP",PranaPada(jataka.ishta,jataka.chart.surya))
+
