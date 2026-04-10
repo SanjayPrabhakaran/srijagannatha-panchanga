2b44249b9433bdd0ca9407932da73d2221b35cb9
Sanjay Prabhakaran <sanjaychettiar@gmail.com>
2026-04-10 14:14:11 +0530

    latest Android APks

---
+++ SJPv6/env.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export ANDROID_SDK_ROOT=/home/sanjay/Android/Sdk
+export ANDROID_HOME=/home/sanjay/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/30.0.3
+
